72a5946addb9a4c09578d38b09fe70d56adba443
Felipe0724 <felipepala0724@gmail.com>
2024-02-20 16:45:42 -0500

    Seguimos practicando js

--- Javascript/scripts/clase3.js
+++ Javascript/scripts/clase3.js
@@ -2,5 +2,28 @@ let PI = 3.1416;
 let HolaMundo = "Hola Mundo";
 console.log(PI,HolaMundo);
 
+let name = "Felipe";
+console.log(name);
+
+let e = 3.1425;
+console.log(e);
+
 let suma = PI+HolaMundo;
 console.log(suma);
+
+PI === name? console.log("Verdadero"):console.log("Falso");
+
+e===PI? console.log("Verdadero"):console.log("Falso");
+
+const vidasIniciales = 30;
+
+let verificaciones = 4;
+if(verificaciones != 4){
+    console.log("Es diferente a la verificación");
+}else{
+    console.log("La verificación es correcta");
+}
+
+/*Creación de variables sin asignación inicial de valores*/
+
+let playerName;
