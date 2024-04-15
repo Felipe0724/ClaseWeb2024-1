bf72bd9c24046cad04601184919f37715e53fdbc
Felipe0724 <felipepala0724@gmail.com>
2024-04-15 08:32:26 -0500

    Creando el dom

---
+++ Clase2/css/dom.js
@@ -0,0 +1,31 @@
+import { crearDiv } from "./div.js";
+
+// Función para crear y agregar un botón al cuerpo del documento
+const crearBoton = (texto) => {
+    const boton = document.createElement('button');
+    boton.className = 'profile__btn';
+    boton.innerHTML = `<p>${texto}</p>`;
+    return boton;
+};
+
+// Crear el contenido de la página
+const contenido = `
+<main class="profile">
+    <section class="informacion">
+        <img src="./assets/mujer.jpg" alt="Jessica Randall - profile photo" class="profile__avatar">
+        <h1 class="profile__name"><p>Jessica Randall</p></h1>
+        <p class="profile__city">London, United Kingdom</p>
+        <p class="profile__description">"Front-end developer and avid reader."</p>
+    </section>
+    <section class="buttons">
+        ${crearBoton('GitHub').outerHTML}
+        ${crearBoton('Frontend Mentor').outerHTML}
+        ${crearBoton('LinkedIn').outerHTML}
+        ${crearBoton('Twitter').outerHTML}
+        ${crearBoton('Instagram').outerHTML}
+    </section>
+</main>
+`;
+
+// Crear y agregar el contenido al cuerpo del documento
+document.body.appendChild(crearDiv(contenido));
