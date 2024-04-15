bb65e4ade718aa1a8c2a7c851bd222d3c2e8a8c1
Felipe0724 <felipepala0724@gmail.com>
2024-04-15 08:19:09 -0500

    Programando el DOM

---
+++ Clase2/css/dom.js
@@ -0,0 +1,6 @@
+const descripcion = document.createElement('p');
+descripcion.textContent = 'Desarrolladora web, programadora y diseñadora de logos';
+descripcion.classList.add('informacion');
+document.body.appendChild(descripcion);
+
+
